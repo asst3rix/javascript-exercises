1be0e81b40a0ae047efc13b4ba0256225c0b81b6
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-03-03 17:28:20 +0100

    Do exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(expression) 
+{
+    let result = "";
+    for (let i = expression.length - 1; i >= 0; i--)
+    {
+        result += expression.at(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
