523063a4c5a229c54145f6ab151b1c4326042a40
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-03-04 17:06:37 +0100

    Do exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if (min < 0
+     || max < 0 
+     || !Number.isInteger(min) 
+     || !Number.isInteger(max) 
+     || typeof min !== "number" 
+     || typeof max !== "number") {
+        return "ERROR";
+    }
+    else {
+        if (min > max) {
+            let temp = max;
+            max = min;
+            min = temp;
+        }
 
+        let sum = 0;
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
