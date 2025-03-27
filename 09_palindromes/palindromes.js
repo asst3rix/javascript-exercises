f283fa1a440a89a3b8c9846170b78b0105db1763
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-03-27 17:30:20 +0100

    Do exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (originalString) 
+{
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    // On nettoie le string d'origine sans la ponctuation, les chiffres et les majuscules
+    const cleanedString = originalString.toLowerCase()
+                                        .split('')
+                                        .filter((character) => alphanumerical.includes(character))
+                                        .join('');
+    
+    // On inverse l'ordre des caractères et on le sauvegarde pour comparaison
+    const reversedString = cleanedString.split('').reverse().join('');
 
+    // On test sur la version non inversée nettoyée, et on retourne le résultat (true ou false)
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
