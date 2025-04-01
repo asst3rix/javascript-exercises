2c82d50cb97ef9ec6d5871a3f90f70dd6eeb5aee
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-04-01 17:51:22 +0200

    Do exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    const oldest = array.sort(function (a, b) {
+        if (a.yearOfDeath === undefined) { a.yearOfDeath = new Date().getFullYear(); }
+        if (b.yearOfDeath === undefined) { b.yearOfDeath = new Date().getFullYear(); }
 
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
