011fab2126b7f2a591c503e36a6e315482af7041
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-04-01 16:59:19 +0200

    Do exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if (number === 0 || number === "0") {
+        return 0;
+    } else if (number < 0) {
+        return "OOPS";
+    } else {
+        const fibo = [1, 1];
+        for (let i = 2; i < number; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        return fibo[number - 1];
+    }
 };
 
 // Do not edit below this line
